0334d406b0a5a1ea700f65e5a55e1cf39ed486c0
Juan Cruz <juan.cruz.gutierrez.saravia@gmail.com>
2023-10-26 14:45:30 -0300

    Nuevo componente para renderizado de lista

---
+++ frontend/src/components/WebsitesList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container mt-5">
+    <h1>Sitos web generados</h1>
+    <button class="btn btn-dark">Agregar nuevo sitio</button>
+    <table class="table table-hover mt-5">
+      <thead class="table-dark">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nombre del sitio</th>
+          <th scope="col">URL</th>
+          <th scope="col">Profundidad</th>
+          <th scope="col">Frecuencia</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(website, index) in websites">
+          <th scope="row">{{ index }}</th>
+          <td>{{ website.nombre }}</td>
+          <td>{{ website.url }}</td>
+          <td>{{ website.niveles }}</td>
+          <td>{{ website.frecuencia }}</td>
+          <td> <button class="btn btn-sm btn-dark px-3">Editar</button> </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default{
+    props:{
+      websites:{
+        type:Array,
+        required: true
+      }
+    }
+  }
+</script>
--- frontend/src/views/InicioView.vue
+++ frontend/src/views/InicioView.vue
@@ -1,11 +1,9 @@
 <script setup>
     import WebSiteService from '../services/WebsiteServiceClass'
-    import AppHeader from '../components/AppHeader.vue';
-
     import { ref } from 'vue'
     import { onBeforeMount } from 'vue'
     import { useRoute } from 'vue-router'
-
+    
     const websites = ref([])
 
     const setWebsites = () => {
@@ -15,34 +13,14 @@
     }
 
     onBeforeMount(() => setWebsites());
-    console.log(websites.value)
 </script>
 
 <template>
     <AppHeader/>
-    <div class="container mt-5">
-        <h1>Sitos web generados</h1>
-        <div class="row">
-            <div class="col-12">
-                <table class="table">
-                    <thead class="table-dark">
-                        <tr>
-                            <th scope="col">Nombre del sitio</th>
-                            <th scope="col">URL</th>
-                            <th scope="col">Profundidad</th>
-                            <th scope="col">Frecuencia</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="website in websites">
-                            <td>{{ website.nombre }}</td>
-                            <td>{{ website.url }}</td>
-                            <td>{{ website.niveles }}</td>
-                            <td>{{ website.frecuencia }}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
+    <WebsitesList :websites="websites"/>
 </template>
+
+<script>
+    import AppHeader from '../components/AppHeader.vue';
+    import WebsitesList from '../components/WebsitesList.vue';
+</script>
