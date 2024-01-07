fefd59e04978b337fa63f75437ed7bb7e7149aef
Juan Cruz <juan.cruz.gutierrez.saravia@gmail.com>
2024-01-07 18:29:33 -0300

    Agrego controlador de tareas

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -2,3 +2,4 @@ export * from './ping.controller';
 export * from './sitio-tarea.controller';
 export * from './tarea-snapshot.controller';
 export * from './sitio.controller';
+export * from './tarea.controller';
