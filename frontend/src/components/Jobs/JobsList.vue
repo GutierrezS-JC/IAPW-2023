0a41c67e909ac1baeb5d063d4ee4b9af74c89734
Juan Cruz <juan.cruz.gutierrez.saravia@gmail.com>
2024-01-13 21:18:42 -0300

    Eliminacion de tarea y snapshots vinculados

--- frontend/src/components/Jobs/JobsList.vue
+++ frontend/src/components/Jobs/JobsList.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from "vue-router";
+import { client } from '../../types/ApiClient';
 import JobsEdit from "./JobsEdit.vue";
+import Swal from 'sweetalert2'
 
 const router = useRouter();
 
@@ -12,10 +14,66 @@ const goToSnapshot = (jobId) => {
   router.push(`/snapshots/${jobId}`)
 }
 
-const handleEdit = (event, job) => {
+// Edit
+const handleSelect = (event, job) => {
   event.stopPropagation();
   jobSelected.value = job
 }
+
+const doNothing = (event) => {
+  event.stopPropagation()
+}
+
+const handleDelete = (event, jobId) => {
+  event.stopPropagation();
+
+  Swal.fire({
+    title: '¿Estas seguro?',
+    text: "Estas a punto de eliminar la tarea y con ella todos sus snapshots (capturas) de forma permanente",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Si, eliminar!',
+    cancelButtonText: 'No, cancelar',
+  }).then(async (result) => {
+    if (result.isConfirmed) {
+      await client['TareaSnapshotController.delete'](jobId)
+        .then(async () => {
+          console.log('Snapshots eliminados');
+
+          await client['TareaController.deleteById'](jobId)
+            .then(() => {
+              Swal.fire(
+                'Exito',
+                'Se ha eliminado la tarea y sus correspondientes capturas',
+                'success'
+              );
+            })
+            .catch(error => {
+              console.log(error);
+              Swal.fire(
+                'Error',
+                'No se pudo eliminar la tarea',
+                'error'
+              );
+            })
+            .finally(() => {
+              props.resetList()
+            });
+        })
+        .catch(e => {
+          console.log(e);
+          Swal.fire(
+            'Error',
+            'No se pudo eliminar el sitio',
+            'error'
+          );
+        });
+    }
+  });
+};
+
 
 const props = defineProps({
   websiteJobs: Array,
@@ -58,12 +116,13 @@ const props = defineProps({
           </td>
           <td>
             <span>
-              <i class="bi bi-pencil-square a-option fs-4"
-                data-bs-target="#modal-edit-job-form" 
-                data-bs-toggle="modal"
-                @click="handleEdit($event, job)"
-              ></i>
-              <i class="bi bi-trash3-fill ms-3 a-option fs-4"></i>
+              <i v-if="job.estado === 'En proceso'" class="bi bi-pencil-square a-option fs-4"
+                style="color: #21252958; cursor: default;" @click="doNothing($event)"></i>
+              <i v-else class="bi bi-pencil-square a-option fs-4" data-bs-target="#modal-edit-job-form"
+                data-bs-toggle="modal" @click="handleSelect($event, job)"></i>
+              <i v-if="job.estado === 'En proceso'" class="bi bi-trash3-fill ms-3 fs-4"
+                style="color: #21252958; cursor: default;" @click="doNothing($event)"></i>
+              <i v-else class="bi bi-trash3-fill ms-3 a-option fs-4" @click="handleDelete($event, job.id)"></i>
             </span>
           </td>
         </tr>
