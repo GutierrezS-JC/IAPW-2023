8f7d6ca760eabd5e9016d8b47c03aea9ffdfbf13
Juan Cruz <juan.cruz.gutierrez.saravia@gmail.com>
2024-01-15 12:03:23 -0300

    Test completo de sitios, tareas, snapshots y busqueda

--- frontend/cypress/e2e/websiteTasksSnapshots.cy.js
+++ frontend/cypress/e2e/websiteTasksSnapshots.cy.js
@@ -1,11 +1,11 @@
-describe('Creacion de un sitio web, edicion, cambio de estado (deshabilitado) y eliminacion', () => {
+describe('Creacion de sitio, tareas y snapshots', () => {
   beforeEach(() => {
     cy.viewport('macbook-16')
     cy.loginToAuth0ViaSocial('google')
     cy.visit('http://localhost:5173/inicio')
   })
 
-  it('Agregar un nuevo sitio, verificar en la lista, deshabilitar, editar y eliminar.', () => {
+  it('Agregar un nuevo sitio, revisar tareas registradas, ver snapshots y realizar busqueda.', () => {
     // Creacion
     cy.get('#agregar-nuevo-sitio').click();
     cy.wait(1000);
@@ -57,18 +57,30 @@ describe('Creacion de un sitio web, edicion, cambio de estado (deshabilitado) y 
     // Mientras vemos las tareas de un sitio registrado, se seguiran registrando tambien para el segundo sitio
     cy.wait(70000);
 
+    // Deshabilitamos los sitios creados recientemente
+    for (let i = 0; i < 2; i++) {
+      cy.get('.website-item-list').eq(i).find('.bi-toggle-on').click();
+      cy.wait(1000);
+      cy.contains('button', 'Si, confirmar!').click();
+      cy.wait(1000);
+      cy.contains('button.swal2-confirm', 'OK').click();
+    }
+
+    // Verificamos que esten efectivamente detenidos (inhabilitados)
+    cy.get('.website-item-list .bi-toggle-on').should('not.exist');
+
     // Ver tareas
     cy.get('.website-item-list').last().find('.btn-dark').click();
 
-    // Esperamos al menos 4 segundos por las dudas a esperar la respuesta del servidor
-    cy.wait(4000);
+    // Esperamos al menos 3 segundos por las dudas a esperar la respuesta del servidor
+    cy.wait(3000);
 
     // Ver Snapshots
     // Verificamos que exista al menos un elemento en la lista. 
     // El mas nuevo es el ultimo registrado en la lista. Hacemos click.
     cy.get('table tbody tr').last().should('exist').click();
+    cy.wait(1000);
 
-    cy.wait(5000);
 
     // Detalles de snapshot (captura) - Seleccionar
     // Verificamos que exista al menos un snapshot segun nuestro test y hacemos click en el primero
@@ -87,16 +99,47 @@ describe('Creacion de un sitio web, edicion, cambio de estado (deshabilitado) y 
     // Volver al inicio
     cy.get('#main-title-link').click();
 
-    // Deshabilitamos los sitios
-    cy.get('.website-item-list').each(($websiteItem) => {
-      const $toggleIcon = $websiteItem.find('.bi-toggle-on');
-      cy.wrap($toggleIcon).click();
-      cy.wait(1000)
-      cy.contains('button', 'Si, confirmar!').click();
-      cy.wait(1000)
-      cy.contains('button.swal2-confirm', 'OK').click();
-      cy.wait(2000)
-    })
-
+    // Cerramos sesion
+    cy.wait(1000);
+    cy.get('#cerrar-sesion-button').click();
+    cy.url().should('eq', 'http://localhost:5173/');
   })
+
+  it('Editar y eliminar tarea. Finalmente eliminar los sitios registrados', () => {
+    // Entramos nuevamente a revisar las tareas del sitio
+    cy.get('.website-item-list').last().find('.btn-dark').click();
+
+    // Editamos una tarea
+    cy.get('.bi.bi-pencil-square').first().click();
+    cy.get('#name-edit-job-form').type('Nuevo nombre');
+    cy.get('#confirmar-editar-job').click();
+    cy.contains('button', 'OK').click();
+    cy.get('.table-responsive table tbody tr:first-child td:nth-child(2)').should('have.text', 'Nuevo nombre');
+    cy.wait(1000);
+    cy.get('.bi.bi-trash3-fill').first().click();
+    cy.wait(1000);
+    cy.contains('button.swal2-confirm', 'Si, eliminar!').click();
+    cy.contains('button.swal2-confirm', 'OK').click();
+    cy.get('.table-responsive table tbody').should('be.empty');
+
+    // Volver al inicio
+    cy.get('#main-title-link').click();
+
+    for (let i = 0; i < 2; i++) {
+      cy.get('.website-item-list').first().find('.bi.bi-trash3-fill').click();
+      cy.wait(1000)
+      cy.contains('button', 'Si, eliminar!').click();
+      cy.wait(1000)
+      cy.get('button.swal2-confirm.swal2-styled').click();
+    }
+    
+    // No hay mas sitios
+    cy.get('.website-item-list').should('not.exist');
+
+    // Cerramos sesion
+    cy.wait(1000);
+    cy.get('#cerrar-sesion-button').click();
+    cy.url().should('eq', 'http://localhost:5173/');
+  })
+
 })
